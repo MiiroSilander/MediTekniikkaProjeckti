78c43106d34257fda5873bb6db948622afe3c9d2
MiiroSilander <miiro.silander@gmail.com>
2021-04-28 15:59:09 +0300

    top ja side bar asettelut, tyhjä html2 sivu.

--- projecktikansio/Script.js
+++ projecktikansio/Script.js
@@ -1,7 +1,15 @@
 // JavaScript source code
 
 
+/* Kuinka paljon side bar liukuu sivun p‰‰lle  xxxPX tai xxx%*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "450px";
+}
 
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+} 
 
 
 
