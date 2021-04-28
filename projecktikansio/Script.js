b823846d8f6c72b37307bbf7c7154d480c599a34
Henri Konttinen <33953308+Zarrow666@users.noreply.github.com>
2021-04-28 17:04:27 +0300

    Add files via upload

--- projecktikansio/Script.js
+++ projecktikansio/Script.js
@@ -1,9 +1,9 @@
 // JavaScript source code
+const apiurl = "https://api.igdb.com/v4/games";
 
-
-/* Kuinka paljon side bar liukuu sivun päälle  xxxPX tai xxx%*/
+/* Kuinka paljon side bar liukuu sivun pï¿½ï¿½lle  xxxPX tai xxx%*/
 function openNav() {
-    document.getElementById("mySidenav").style.width = "450px";
+    document.getElementById("mySidenav").style.width = "200px";
 }
 
 /* Set the width of the side navigation to 0 */
@@ -11,6 +11,30 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 } 
 
+const searchButton = document.getElementById("searchbutton");
+const searchField = document.getElementById("search");
 
+searchButton.addEventListener('click', doQuery);
 
+function doQuery() {
+    let searchWord = searchField.value;
+    
+    apiQuery = apiurl + searchWord;
+    console.log("LÃ¤hetettÃ¤vÃ¤ kysely: " + apiQuery);
 
+    doSearch(apiQuery);        
+}
+
+function doSearch(apiQuery)  {
+
+    fetch(apiQuery).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        showResult(json);
+    });
+};
+
+function showResult(jsonData) {
+    
+    console.log(jsonData);
+}
